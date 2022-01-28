61810debcf480a0629aba8368a52600e586f9984
ajakhanwal <97641897+ajakhanwal@users.noreply.github.com>
2022-01-28 14:38:22 -0800

    Add files via upload

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,74 +1,28 @@
-// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Stack;
-
-public class MarkdownParse {
-
-    public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        
-        int currentIndex = 0;
-        Stack<Character> bracketTracker = new Stack<>(); 
-        boolean findLink = false;
-        int start = 0;
-        int end = 0;
-        while (currentIndex < markdown.length()) {
-            char curr = markdown.charAt(currentIndex);
-            //if an escape char is found, skip it and the 
-            //character it is escaping
-            if (curr == '\\') {
-                currentIndex += 2;
-                continue;
+    import java.io.IOException;
+    import java.nio.file.Files;
+    import java.nio.file.Path;
+    import java.util.ArrayList;
+    
+    public class MarkdownParse {
+        public static ArrayList<String> getLinks(String markdown) {
+            ArrayList<String> toReturn = new ArrayList<>();
+            // find the next [, then find the ], then find the (, then take up to
+            // the next )
+            int currentIndex = 0;
+            while(currentIndex < markdown.length()) {
+                int nextOpenBracket = markdown.indexOf("[", currentIndex);
+                int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+                int openParen = markdown.indexOf("(", nextCloseBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
             }
-            //if we are potentially looking at a link with []
-            if (findLink) {
-                // if there arent any other brackets on the bracket tracker
-                if (bracketTracker.isEmpty()) {
-                    if (curr == '(') {
-                        bracketTracker.push(curr);
-                        start = currentIndex;
-                    } else { //something else came after the ] that wasn't (
-                        findLink = false;
-                    }
-                } else {
-                    if (curr == ')') {
-                        end = currentIndex;
-                        toReturn.add(markdown.substring(start + 1, end));
-                        bracketTracker.pop();
-                        findLink = false;
-                    }
-                }
-            } else {
-                if (curr == '[') {
-                    bracketTracker.push(curr);
-                } else if (curr == ']') {
-                    if (!bracketTracker.isEmpty()) {
-                        bracketTracker.clear();
-                        findLink = true;
-                    }
-                } else if (curr == '!') {
-                    if (currentIndex < markdown.length() - 1 && markdown.charAt(currentIndex + 1) == '[') {
-                        currentIndex += 2;
-                    }
-                }
-            }
-            // move to next char
-            currentIndex++;
+            return toReturn;
         }
-
-        return toReturn;
+        public static void main(String[] args) throws IOException {
+            Path fileName = Path.of(args[0]);
+            String contents = Files.readString(fileName);
+            ArrayList<String> links = getLinks(contents);
+            System.out.println(links);
+        }
     }
-    public static void main(String[] args) throws IOException {
-        // take in the first command line arg as the file name to be searched for links.
-		Path fileName = Path.of(args[0]);
-        // read the file contents into a string
-	    String contents = Files.readString(fileName);
-        // run getLinks on the contents of the file
-        ArrayList<String> links = getLinks(contents);
-        // print out the links that we found
-        System.out.println(links);
-    }
-}
